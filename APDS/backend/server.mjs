51d4e019be3895d5e78ef4e5e63860db4eb13769
TanyaGov <ST10032085@vcconnect.edu.za>
2024-09-17 21:29:20 +0200

    ICE Task 3

---
+++ APDS/backend/server.mjs
@@ -0,0 +1,36 @@
+import https from "https"; 
+import http from "http"; 
+import fs from "fs"; 
+import posts from "./routes/post.mjs"; 
+import users from "./routes/user.mjs"; 
+import express from "express"; 
+import cors from "cors"; 
+
+const PORT = 3001; 
+const app = express(); 
+
+const options = { 
+    key: fs.readFileSync('keys/privatekey.pem'), 
+    cert: fs.readFileSync('keys/certificate.pem') 
+}; 
+
+app.use(cors()); 
+app.use(express.json()); 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', '*'); 
+    next(); }); 
+
+    app.get('/test', (req, res)=> {
+        res.send("I am working")
+    })
+    
+    app.use("/post", posts); 
+    app.route("/post", posts); 
+    app.use("/user", users); 
+    app.route("/user", users); 
+    
+    let server = https.createServer(options, app); 
+    console.log(PORT); 
+    server.listen(PORT);
